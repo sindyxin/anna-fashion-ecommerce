c64cea68c441a6d5ee903fe2cf865673969c73ae
anna <sindyxin1@gmail.com>
2020-03-08 21:39:12 -0700

    create project with create react app and added components with styling for homepage

---
+++ src/pages/HomePage/script.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './styles.scss';
+import { Directory } from '../../components/Directory/script.jsx';
+
+export const HomePage = () => (
+	<div className='home-page'>
+		<Directory></Directory>
+	</div>
+);
